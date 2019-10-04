3cfb9fe629a8e5bd870dcde29302bbe7b058e6c4
Jr Persico <jrpersico@Jrs-Mac-mini.local>
2019-10-04 11:13:35 +0200

    v0.9

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -59,7 +59,7 @@ methods: {
     background-size: 34%;
     background-position: 90% 82%;
   }
-  @media (min-width: 768px) and (max-width: 1200px) {
+  @media (min-width: 768px) and (max-width: 1527px) {
     background-image: url('../assets/surveillance.svg');
     background-repeat: no-repeat;
     background-size: 16%;
