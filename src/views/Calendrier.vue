cd68cc5426a79393efb1882fee5b754e21c0329a
Jr Persico <jrpersico@Jrs-Mac-mini.local>
2019-10-15 17:49:51 +0200

    v0.16

--- src/views/Calendrier.vue
+++ src/views/Calendrier.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="calendrier">
-    <NavBar></NavBar>
-    <b-container class="profile-container">
-      <Profile></Profile>
+<div class="calendrier">
+  <NavBar></NavBar>
+  <b-container class="profile-container">
+    <Profile></Profile>
+  </b-container>
+  <b-container class="menu-container">
+    <Menu></Menu>
+  </b-container>
+  <b-container class="content-page" fluid>
+    <b-container>
+      <b-row>
+        <b-col cols="12">
+          <div class="card-content-page calendar">
+            <vue-cal style="height: 600px" default-view="month" events-on-month-view="short" :disable-views="['years', 'year', 'day']" :events="events" :time-from="8 * 60" :time-to="23 * 60" :time-step="60" locale="fr"></vue-cal>
+          </div>
+        </b-col>
+      </b-row>
     </b-container>
-    <b-container class="menu-container">
-      <Menu></Menu>
-    </b-container>
-    <b-container class="content-page" fluid>
-      <b-container>
-        <b-row>
-          <b-col cols="12">
-            <div class="card-content-page calendar">
-              <vue-cal style="height: 600px" default-view="month" today-button :events="events" :time-from="8 * 60" :time-to="23 * 60" :time-step="60"></vue-cal>
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-    </b-container>
-    <Footer></Footer>
-  </div>
+  </b-container>
+  <Footer></Footer>
+</div>
 </template>
 
 <script>
@@ -28,6 +28,7 @@ import Profile from '@/components/Profile'
 import Menu from '@/components/Menu'
 import Footer from '@/components/Footer'
 import VueCal from 'vue-cal'
+import 'vue-cal/dist/i18n/fr.js'
 import 'vue-cal/dist/vuecal.css'
 
 export default {
@@ -41,26 +42,22 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          start: '2019-10-21 10:30',
-          end: '2019-10-21 11:30',
+      events: [{
+          start: '2019-10-21 11:30',
+          end: '2019-10-21 14:30',
           title: 'Need to go shopping',
-          content: '<i class="v-icon material-icons">shopping_cart</i>',
           class: 'leisure'
         },
         {
           start: '2019-10-21 09:30',
-          end: '2019-10-21 10:30',
+          end: '2019-10-21 11:30',
           title: 'Golf with John',
-          content: '<i class="v-icon material-icons">golf_course</i>',
           class: 'sport'
         },
         {
           start: '2019-10-22 10:30',
-          end: '2019-10-22 11:30',
+          end: '2019-10-22 12:30',
           title: 'Dad\'s birthday!',
-          content: '<i class="v-icon material-icons">cake</i>',
           class: 'sport'
         }
       ]
