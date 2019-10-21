542b6b97719ce40d96ed49bef81dbfc417acbae2
Jr Persico <jrpersico@Jrs-Mac-mini.local>
2019-10-21 12:10:02 +0200

    v0.18

--- src/views/Compte.vue
+++ src/views/Compte.vue
@@ -10,9 +10,9 @@
     <b-container class="content-page" fluid>
       <b-container>
         <b-row>
-          <b-col cols="12" sm="12" md="7" lg="7">
+          <b-col cols="12" sm="12" md="12" lg="7">
             <div class="card-content-page optin-card">
-              <h5 class="mb-3">Mes informations</h5>
+              <h5 class="mb-4">Mes informations</h5>
               <b-form>
                 <b-row>
                   <b-col cols="6">
@@ -65,37 +65,52 @@
                       <b-form-input id="phone" v-model="form.phone" type="text" required></b-form-input>
                     </b-form-group>
                   </b-col>
+                  <b-col cols="12">
+                      <b-button type="submit" class="px-4" pill>Mettre à jour</b-button>
+                  </b-col>
                 </b-row>
-
-                <b-button type="submit" variant="primary">Mettre à jour</b-button>
               </b-form>
             </div>
           </b-col>
-          <b-col cols="12" sm="12" md="5" lg="5">
+          <b-col cols="12" sm="12" md="12" lg="5">
             <div class="card-content-page optin-card">
-              <h5>Changer mon mot de passe</h5>
+              <h5 class="mb-4">Changer mon mot de passe</h5>
               <b-form>
-                <b-form-group label="Téléphone :" label-for="phone">
-                  <b-form-input id="phone" v-model="form.phone" type="text" required></b-form-input>
-                </b-form-group>
-                <b-form-group label="Téléphone :" label-for="phone">
-                  <b-form-input id="phone" v-model="form.phone" type="text" required></b-form-input>
-                </b-form-group>
-
-                <b-button type="submit" variant="primary">Mettre à jour</b-button>
+                <b-row>
+                  <b-col cols="12">
+                    <b-form-group label="Ancien Mot de passe :" label-for="phone">
+                      <b-form-input id="phone" v-model="form.phone" type="password" required></b-form-input>
+                    </b-form-group>
+                  </b-col>
+                  <b-col cols="12">
+                    <b-form-group label="Nouveau Mot de passe :" label-for="phone">
+                      <b-form-input id="phone" v-model="form.phone" type="password" required></b-form-input>
+                    </b-form-group>
+                  </b-col>
+                  <b-col cols="12">
+                      <b-button type="submit" class="px-4" pill>Mettre à jour</b-button>
+                  </b-col>
+                </b-row>
               </b-form>
             </div>
             <div class="card-content-page optin-card">
-              <h5>Mes alertes</h5>
+              <h5 class="mb-4">Mes alertes</h5>
               <b-form>
-                <b-form-checkbox id="checkbox-offre" v-model="offre" name="checkbox-offre">
-                  Les offres commerciales de l'Olympique Lyonnais
-                </b-form-checkbox>
-                <b-form-checkbox id="checkbox-partners" v-model="partners" name="checkbox-partners">
-                  Les offres partenaires de l'Olympique Lyonnais
-                </b-form-checkbox>
-
-                <b-button type="submit" variant="primary">Mettre à jour</b-button>
+                <b-row>
+                  <b-col cols="12">
+                    <b-form-checkbox class="mb-3" id="checkbox-offre" v-model="form.offre" name="checkbox-offre">
+                      Les offres commerciales de l'Olympique Lyonnais
+                    </b-form-checkbox>
+                  </b-col>
+                  <b-col cols="12">
+                    <b-form-checkbox class="mb-3" id="checkbox-partners" v-model="form.partners" name="checkbox-partners">
+                      Les offres partenaires de l'Olympique Lyonnais
+                    </b-form-checkbox>
+                  </b-col>
+                  <b-col cols="12">
+                      <b-button type="submit" class="px-4" pill>Mettre à jour</b-button>
+                  </b-col>
+                </b-row>
               </b-form>
             </div>
           </b-col>
